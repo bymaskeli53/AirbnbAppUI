b7330f6732b0af1f653d1e27d040cad2f392f364
Muhammet Emin Gündoğar <muhammetemingundogar53@gmail.com>
2025-06-25 12:35:12 +0300

    Update MainActivity.kt

--- app/src/main/java/com/gundogar/airbnbapp/MainActivity.kt
+++ app/src/main/java/com/gundogar/airbnbapp/MainActivity.kt
@@ -10,13 +10,6 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-       // WindowCompat.setDecorFitsSystemWindows(window, false)
-       // enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-    }
+ }
 }
